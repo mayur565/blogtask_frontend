d2e59e0d4bc48768a5748882966c6f4519e89750
mayur565 <badgujarmayur565@gmail.com>
2025-07-03 15:08:17 +0530

    check helth api

--- src/app/blog-form/page.js
+++ src/app/blog-form/page.js
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import BlogFormPage from "@/component/blog/blogForm";
-
+// export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading Blog Form...</div>}>
